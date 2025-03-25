9932436c9fb15aecb3be0138cd6e720ca0094531
Brian Evans <evansbr@si.edu>
2025-03-25 17:13:19 -0400

    added question 5

--- problem_sets/problem_set_3/problem_set_3_playdate.R
+++ problem_sets/problem_set_3/problem_set_3_playdate.R
@@ -9,15 +9,45 @@ library(tidyverse)
 
 # 3. Read in the data and send to the global environment
 
-read_rds("caterpillars_count.rds") %>% 
+read_rds("data/raw/caterpillars_count.rds") %>% 
   list2env(.GlobalEnv)
 
 # 4. How many caterpillars has Caterpillars Count counted? Please provide your
 # answer as a one-value numeric vector.
 
-cc_observations %>% 
+observations %>% 
   filter(arthropod == "caterpillar") %>% 
   pull(arthropod_quantity) %>% 
   sum()
 
-# 
+# 5. Please make a summary data frame of the number of sites in the District of
+# Columbia, Maryland, and Virginia and arrange the table alphabetically by 
+# region:
+
+sites %>% 
+  filter(
+    region %in%
+      c(
+        "DC",
+        "MD", 
+        "VA"
+      )
+  ) %>% 
+  summarize(
+    n = n(),
+    .by = region
+  ) %>% 
+  arrange(region)
+
+
+
+observations %>% 
+  filter(arthropod == "caterpillar")
+
+observations %>% 
+  filter(arthropod == "caterpillar") %>% 
+  distinct(survey_id)
+summarize(
+  n_caterpillar = n(),
+  .by = survey_id
+) %>% summary()
