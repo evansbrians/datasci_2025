bac3bc4b96a3eb6379fc3b12ddecfdefe397d28d
Brian Evans <evansbr@si.edu>
2025-03-26 10:38:43 -0400

    student version of ps3

--- problem_sets/problem_set_3/problem_set_3.R
+++ problem_sets/problem_set_3/problem_set_3.R
@@ -1,127 +1,80 @@
-# Problem set 3 key: Global CO2 
 
-# 2 -----------------------------------------------------------------------
+# Answer key for problem set 3: Counting caterpillars
 
-# Attach the core tidyverse packages to your current R session:
+# question 1 --------------------------------------------------------------
 
+# Before opening your script file for this problem set, change the name of
+# the `problem_set_3.R` to "problem_set_3_[last name]_[first name].R" using
+# a snake case naming convention. *Note: You will submit this script file 
+# as your assignment*.
 
+# question 2 --------------------------------------------------------------
 
-# 3 -----------------------------------------------------------------------
-
-# The following code reads in world_bank_countries.csv and assigns the name
-# countries to your global environment. Please modify this code such that it
-# follows best management practices in code formatting.
-
-# Given:
-
-countries=read_csv("data/raw/world_bank_countries.csv")%>%select(!country_region)
-
-# Answer:
+# Open the script file in RStudio and attach the tidyverse metapackage to 
+# your current R session.
 
 
 
-# 4, given ----------------------------------------------------------------
+# question 3 --------------------------------------------------------------
 
-# The code below reads in and wrangles the data successfully, but not
-# parsimoniously. Modify the code such that the process below is completed in a
-# chained analysis in which only the name associated with the final object,
-# `world_pop`, is assigned to your global environment (i.e., the code block
-# should not include intermediate assignments).
-
-# Read in population data:
-
-world_pop_raw <-
-  read_csv(
-    'data/raw/API_SP.POP.TOTL_DS2_en_csv_v2_2763937.csv', 
-    skip = 3)
-
-# Subset to columns of interest:
-
-world_pop_column_subset <-
-  select(
-    world_pop_raw,
-    
-    # Rename Country Code column to remove space:
-    
-    country_code = `Country Code`,
-    
-    # Subset to other columns of interest:
-    
-    `1960`:`2020`)
-
-
-# Make the data tidy by pivoting to a long form table:
-
-world_pop_long <-
-  pivot_longer(
-    world_pop_column_subset,
-    names_to = 'year',
-    values_to = 'population',
-    `1960`:`2020`)
-
-# Finally, convert the year column to numeric:
-
-world_pop <-
-  mutate(
-    world_pop_long,
-    year = as.numeric(year))
-
-# 4, answer ---------------------------------------------------------------
-
- 
-
-# 5 -----------------------------------------------------------------------
-
-# In a chained analysis with no intermediate assignments:
-
-# * Read in the CO2 data (API_EN.ATM.CO2E.PC_DS2_en_csv_v2_2764620.csv)
-# * Wrangle the data such that the resultant tibble contains (only) the 
-#   variables `country_code`, `year`, and `co2` (as above)
-# * Convert the class of the `year` variable to numeric
-# * Globally assign the data frame to the name `co2`
-
-  
-
-# 6 -----------------------------------------------------------------------
-
-# Join the population and CO2 tibbles. Globally assign the resultant object to
-# the name `population_co2`.
+# Read in the `caterpillars_count.rds` and assign the names of the list to the
+# global environment.
 
 
 
-# 7 -----------------------------------------------------------------------
+# question 4 --------------------------------------------------------------
 
-# Modify the data frame assigned to `populations_co2` such that `co2` values
-# represent the *total* emissions (Note: the total emissions for a given country
-# is the per capita emissions times the population) and the data frame is:
-
-# * Subset to the year 2018
-# * Arranged from the highest to lowest CO2 total emissions in 2018
-# * Subset to only countries with records in the countries data frame
-# * Subset to  the 5 countries with the most CO2 emissions (and also satisfy
-#   the conditions above)
-# * Subset to only the fields `country_code`, `country_name`, and `total_co2`
-# * Globally assigned to the name `top_emitters_2018`
+# How many caterpillars has Caterpillars Count counted? Please provide your
+# answer as a one-value numeric vector.
 
 
 
-# 8 -----------------------------------------------------------------------
+# question 5 --------------------------------------------------------------
 
-# Using `population_co2` and `top_emitters_2018`, generate a ggplot that shows
-# the per capita CO~2~ emissions from 1960 to 2018 for the five countries that
-# had the highest total CO~2~ emissions in 2018. Plot the data such that:
+# Subset the `sites` data frame to where `region` is `DC` (District of
+# Columbia), `MD` (Maryland), or `VA` (Virginia and globally assign the name
+# `sites_dmv` to the resultant object.
 
-# * The data being plotted are the per capita emissions from 1960-2018 for the
-#   top five emitting countries in 2018
-# * Evaluation of the plotting code does not produce any warning messages
-# * Your x-aesthetic is year and is labeled “Year”
-# * Your y-aesthetic is co_2 and is labeled “Carbon dioxide emissions (metric 
-#   tons/person)”
-# * Your color aesthetic is country_name and is labeled “Country”
-# * Your plot includes point and line geometries
-# * The x-axis ranges from 1960 to 2020
-# * The y-axis ranges from 0 to 30
-# * The plot includes a descriptive title
-# * The background of the plot is white and contains no grid lines
+
+
+# question 6 --------------------------------------------------------------
+
+# Create a summary table that displays the number of surveys by year across
+# regions and arrange from highest to lowest number of surveys.
+
+
+
+# question 7 --------------------------------------------------------------
+
+# Subset the survey_locations data frame to those that are located within sites
+# in the District of Columbia, Maryland, or Virginia. For full credit, please
+# complete this such that:
+
+# * No columns are added to, or removed from, `survey_locqtions`;
+# * The `filter` function is not used to subset rows;
+# * The `select` function is not used to subset columns.
+
+
+
+# question 8 --------------------------------------------------------------
+
+# Please generate a summary table that provides the average (mean) number of
+# caterpillars observed in "Beat sheet" and "Visual" surveys across sampling
+# regions. Note that to properly address this question you will have to do
+# something about surveys in which no caterpillars were observed!
+
+
+
+# question 9 --------------------------------------------------------------
+
+# Please generate a bar plot that displays the total number of surveys conducted
+# in the District of Columbia, Maryland, and Virginia in 2024.
+
+
+
+# question 10 --------------------------------------------------------------
+
+# Please generate a summary table that provides the total number of arthropods
+# counted per state in the District of Columbia, Maryland, and Virginia in 2024.
 
 
