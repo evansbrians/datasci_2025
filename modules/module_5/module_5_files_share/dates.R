94ffeb29ae791aea88390a6398128bed0f79f5e7
Brian Evans <evansbr@si.edu>
2025-03-27 14:22:53 -0400

    added the old module 5

---
+++ modules/module_5/module_5_files_share/dates.R
@@ -0,0 +1,51 @@
+
+# setup -------------------------------------------------------------------
+
+library(tidyverse) 
+library(lubridate)
+
+read_rds('data/raw/dates.rds') %>% 
+  list2env(.GlobalEnv)
+
+# good dates --------------------------------------------------------------
+
+good_dates
+
+# bad dates - us dates ----------------------------------------------------
+
+us_dates
+
+# bad dates - european dates ----------------------------------------------
+
+european_dates
+
+# Copy and paste the code block we created above two times. For the first code
+# block, generate a date column using name_month. For the second, create a date
+# column using abbr_month.
+
+
+
+# mixed date format -------------------------------------------------------
+
+test_dates <-
+  mixed_month_names$date[1:6]
+
+# Note: The above could have also been written as:
+
+test_dates <-
+  mixed_month_names %>% 
+  slice(1:6) %>% 
+  pull(date)
+
+# Subset the data frame to just dates that start with numbers. Then replace the
+# date column with a properly formatted date object using the function dmy.
+
+mixed_month_names
+
+# avoiding transitive dependence ------------------------------------------
+
+good_dates
+
+# Without making a new column, filter the date to volcanic eruptions that
+# occurred before 1990. 
+
