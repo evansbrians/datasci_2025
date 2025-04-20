48cafc4e7e6332a8a2b2e1d047623e49a06d1109
Brian <bsevansunc@gmail.com>
2025-04-20 11:31:35 -0400

    formatting of key

--- problem_sets/problem_set_6/problem_set_6_bird_measures_key.R
+++ problem_sets/problem_set_6/problem_set_6_bird_measures_key.R
@@ -19,7 +19,8 @@ read_rds("data/raw/dc_birds.rds") %>%
   tidyselect:::select(
     visits, 
     birds, 
-    measures) %>% 
+    measures
+  ) %>% 
   list2env(.GlobalEnv)
 
 # 4 -----------------------------------------------------------------------
@@ -42,8 +43,10 @@ banding <-
     birds %>% 
       select(
         spp = species, 
-        common_name), 
-    by = "spp") %>% 
+        common_name
+      ), 
+    by = "spp"
+  ) %>% 
   
   # Join visits:
   
@@ -55,7 +58,8 @@ banding <-
     bird_id,
     common_name,
     site_id:date,
-    age:mass)
+    age:mass
+  )
 
 # In a separate code block from the above, remove the names `birds`,
 # `measures`, and `visits` from your global environment.
@@ -106,8 +110,11 @@ banding_subset <-
       age %>% 
       fct_collapse(
         Juvenile = "HY",
-        other_level = "Adult") %>% 
-      fct_relevel(c("Juvenile", "Adult"))
+        other_level = "Adult"
+      ) %>% 
+      fct_relevel(
+        c("Juvenile", "Adult")
+      )
   )
 
 # In a separate code block from the above, remove the name `banding` from 
@@ -170,7 +177,8 @@ species <-
 out_container <-
   vector(
     "list",
-    length = length(species))
+    length = length(species)
+  )
 
 # Iterate across species with a for loop:
 
@@ -178,7 +186,9 @@ for(i in seq_along(species)) {
   out_container[[i]] <-
     banding_subset %>% 
     filter(common_name == species[[i]]) %>% 
-    mutate(mean_mass = mean(mass)) %>% 
+    mutate(
+      mean_mass = mean(mass)
+    ) %>% 
     distinct(common_name, mean_mass)
 }
 
@@ -197,7 +207,9 @@ banding_subset %>%
   purrr::map(
     ~ banding_subset %>% 
       filter(common_name == .x) %>% 
-      mutate(mean_mass = mean(mass)) %>% 
+      mutate(
+        mean_mass = mean(mass)
+      ) %>% 
       distinct(common_name, mean_mass)
   ) %>% 
   bind_rows()
@@ -212,7 +224,9 @@ banding_subset %>%
     function(x) {
       banding_subset %>% 
         filter(common_name == x) %>% 
-        mutate(mean_mass = mean(mass)) %>% 
+        mutate(
+          mean_mass = mean(mass)
+        ) %>% 
         distinct(common_name, mean_mass)
     }
   ) %>% 
@@ -228,7 +242,9 @@ banding_subset %>%
     \(x) {
       banding_subset %>% 
         filter(common_name == x) %>% 
-        mutate(mean_mass = mean(mass)) %>% 
+        mutate(
+          mean_mass = mean(mass)
+        ) %>% 
         distinct(common_name, mean_mass)
     }
   ) %>% 
@@ -244,7 +260,9 @@ banding_subset %>%
     \(x) {
       banding_subset %>% 
         filter(common_name == x) %>% 
-        mutate(mean_mass = mean(mass))
+        mutate(
+          mean_mass = mean(mass)
+        )
     }
   ) %>% 
   bind_rows() %>% 
@@ -271,7 +289,8 @@ banding_subset %>%
 list.files(
   "data/raw", 
   pattern = "bird.*rds$",
-  full.names = TRUE) %>% 
+  full.names = TRUE
+) %>% 
   
   # Read in the files:
   
@@ -285,8 +304,9 @@ list.files(
   set_names(
     "birds_cicadas_lc",
     "dc_birds",
-    "district_birds.rds",
-    "messy_birds.rds") %>% 
+    "district_birds",
+    "messy_birds"
+  ) %>% 
   
   # Assign each list item to the global environment:
   
