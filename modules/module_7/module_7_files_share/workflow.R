47d2df1a947f208eeb03e529460971699686a4e7
Brian Evans <evansbr@si.edu>
2025-04-26 16:25:12 -0400

    code formatting

--- modules/module_7/module_7_files_share/workflow.R
+++ modules/module_7/module_7_files_share/workflow.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 
 dc_birds <- 
-  read_rds('data/raw/district_birds.rds')
+  read_rds("data/raw/district_birds.rds")
 
 source("scripts/source_script.R")
 
@@ -27,29 +27,35 @@ prep_data <-
 # plot bird counts by diet guild ------------------------------------------
 
 prep_data("counts", "count") %>% 
-
+  
   # Summarize data for plotting:
   
   summarize(
     n_birds = sum(count),
-    .by = diet) %>% 
+    .by = diet
+  ) %>% 
   
   # Plot data:
   
-  ggplot(
-    aes(x = diet,
-        y = n_birds)) +
+  ggplot() +
+  aes(
+    x = diet,
+    y = n_birds
+  ) +
   geom_bar(
     stat = "identity",
     fill = "#dcdcdc",
-    color = "black") +
+    color = "black"
+  ) +
   scale_y_continuous(
     expand = c(0, 0),
-    limits = c(0, 4000)) +
+    limits = c(0, 4000)
+  ) +
   labs(
     title = "Bird counts by diet guild",
     x = "Diet guild",
-    y = "Birds observed") +
+    y = "Birds observed"
+  ) +
   my_theme()
 
 # plot bird counts by foraging guild --------------------------------------
@@ -60,24 +66,30 @@ prep_data("counts", "count") %>%
   
   summarize(
     n_birds = sum(count),
-    .by = foraging) %>% 
+    .by = foraging
+  ) %>% 
   
   # Plot data:
   
-  ggplot(
-    aes(x = foraging,
-        y = n_birds)) +
+  ggplot() +
+  aes(
+    x = foraging,
+    y = n_birds
+  ) +
   geom_bar(
     stat = "identity",
     fill = "#dcdcdc",
-    color = "black") +
+    color = "black"
+  ) +
   scale_y_continuous(
     expand = c(0, 0),
-    limits = c(0, 5000)) +
+    limits = c(0, 5000)
+  ) +
   labs(
     title = "Bird counts by foraging guild",
     x = "Foraging guild",
-    y = "Birds observed") +
+    y = "Birds observed"
+  ) +
   my_theme()
 
 # plot bird mass by diet guild --------------------------------------------
@@ -86,19 +98,23 @@ prep_data("captures", "mass") %>%
   
   # Plot data:
   
-  ggplot(
-    aes(x = diet,
-        y = mass)) +
+  ggplot() +
+  aes(
+    x = diet,
+    y = mass) +
   geom_boxplot(
     fill = "#dcdcdc",
-    na.rm = TRUE) +
+    na.rm = TRUE
+  ) +
   scale_y_continuous(
     expand = c(0, 0),
-    limits = c(0, 125)) +
+    limits = c(0, 125)
+  ) +
   labs(
     title = "Bird mass by diet guild",
     x = "Diet guild",
-    y = "Mass") +
+    y = "Mass"
+  ) +
   my_theme()
 
 # plot bird mass by foraging guild ----------------------------------------
@@ -107,19 +123,21 @@ prep_data("captures", "mass") %>%
   
   # Plot data:
   
-  ggplot(
-    aes(x = foraging,
-        y = mass)) +
+  ggplot() +
+  aes(
+    x = foraging,
+    y = mass) +
   geom_boxplot(
     fill = "#dcdcdc",
-    na.rm = TRUE) +
+    na.rm = TRUE
+  ) +
   scale_y_continuous(
     expand = c(0, 0),
-    limits = c(0, 125)) +
+    limits = c(0, 125)
+  ) +
   labs(
     title = "Bird mass by foraging guild",
     x = "Foraging guild",
-    y = "Mass") +
+    y = "Mass"
+  ) +
   my_theme()
-  
-  
